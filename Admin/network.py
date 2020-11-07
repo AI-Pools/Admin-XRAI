0a4a6dccbff64dbc5f91232133cb5a1085021674
Thytu <valentin.de-matos@epitech.eu>
2020-11-07 09:15:44 +0100

    -admin: remove comment int network.py

--- Admin/network.py
+++ Admin/network.py
@@ -29,26 +29,3 @@ class Network(nn.Module):
     t = self.out(t)
 
     return t
-
-# conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5)
-# conv2 = nn.Conv2d(in_channels=6, out_channels=12, kernel_size=5)
-# fc1 = nn.Linear(in_features=12*13*13, out_features=120)
-
-# train_set, train_labels, test_set, test_labels, val_set, val_labels, BATCH_SIZE = dataset_loader.load_dataset()
-# images = train_set[0]
-# images = np.array(images).astype(np.int32)
-# images = torch.Tensor(images)
-
-# t = images
-# t = conv1(t)
-# t = F.relu(t)
-# t = F.max_pool2d(t, kernel_size=2, stride=2)
-# t = F.relu(conv2(t))
-# t = F.max_pool2d(t, kernel_size=2, stride=2)
-
-# print(t.shape)
-
-# t = t.reshape(-1, 12 * 13 * 13)
-# t = F.relu(fc1(t))
-
-# print(t.shape)
